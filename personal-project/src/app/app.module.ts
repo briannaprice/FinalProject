a2e2c93ab43c7f6e16ab7480dd4b9c743e0e6f4c
Brianna Price <briannaprice@Briannas-MacBook-Air.local>
2020-03-25 15:50:32 -0500

    added new components

--- personal-project/src/app/app.module.ts
+++ personal-project/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatComponent } from './chat/chat.component';
+import { UserComponent } from './user/user.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent,
+    UserComponent,
+    LoginComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
