fe05a3ae4530f317a10841a5bd8fd67139bce0a9
Brianna Price <briannaprice@Briannas-MacBook-Air.local>
2020-03-27 10:52:22 -0500

    updated api

--- personal-project/chat-app-server/api.js
+++ personal-project/chat-app-server/api.js
@@ -2,18 +2,6 @@ const express = require('express');
 const router = express.Router();
 const Pusher = require('pusher');
 
-router.get('/', (req, res) => {
-  res.send('all good');
-});
-
-router.post('/pusher/auth', (req, res) => {
-  console.log('POST to /pusher/auth');
-  const socketId = req.body.socket_id;
-  const channel = req.body.channel_name;
-  const auth = pusher.authenticate(socketId, channel);
-  res.send(auth);
-});
-
 const pusher = new Pusher({
   appId: '970947',
   key: 'd17dd14a110ce066a5e4',
@@ -22,8 +10,17 @@ const pusher = new Pusher({
   encrypted: true
 });
 
-pusher.trigger('my-channel', 'my-event', {
-  "message": "hello world"
+let messages = [];
+
+router.get('/', (req, res) => {
+  res.send('all good');
+});
+
+router.post('/pusher/auth', (req, res) => {
+  const socketId = req.body.socket_id;
+  const channel = req.body.channel_name;
+  const auth = pusher.authenticate(socketId, channel);
+  res.send(auth);
 });
 
 module.exports = router;
